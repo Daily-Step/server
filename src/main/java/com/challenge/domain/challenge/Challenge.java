c9848304589948519e21ee95b410ba465fc2d97a
Eunjin Kim <jennyeunjin@khu.ac.kr>
2024-11-04 08:11:10 +0900

    feat: jpa 엔티티 생성 및 연관관계 매핑

---
+++ src/main/java/com/challenge/domain/challenge/Challenge.java
@@ -0,0 +1,51 @@
+package com.challenge.domain.challenge;
+
+import com.challenge.domain.category.Category;
+import com.challenge.domain.member.Member;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "challenge")
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Challenge {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "challenge_id")
+    private Long id;
+    
+    @Column(name="title",nullable = false,length=50)
+    private String title;
+    
+    @Column(name="content",length=500)
+    private String content;
+    
+    @Column(name="color",nullable = false,length=10)
+    private String color;
+    
+    @Column(name="weekly_goal_count",nullable = false)
+    private Integer weeklyGoalCount;
+    
+    @Column(name="total_goal_count",nullable = false)
+    private Integer totalGoalCount;
+    
+    @Column(name="start_date", nullable = false)
+    private Date startDate;
+    
+    @Column(name="end_date",nullable = false)
+    private Date endDate;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+    
+}
---
+++ src/main/java/com/challenge/domain/member/Member.java
@@ -0,0 +1,48 @@
+package com.challenge.domain.member;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "member")
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Member {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+    
+    @Column(name = "email", nullable = false, length = 100)
+    private String email;
+    
+    @Column(name = "nickname", nullable = false, length = 30)
+    private String nickname;
+    
+    @Column(name = "birth")
+    private Date birth;
+    
+    @Column(name = "gender", length = 10)
+    private String gender;
+    
+    @Column(name = "profile_img", length = 1000)
+    private String profileImg;
+    
+    @Column(name = "is_notification_received", nullable = false)
+    private Boolean isNotificationReceived;
+    
+    @Column(name = "login_type", nullable = false)
+    private String loginType;
+    
+    @Column(name="is_deleted", columnDefinition = "boolean default false")
+    private Boolean isDeleted;
+    
+    @Column(name="deleted_at",length=13)
+    private String deletedAt;
+    
+}
