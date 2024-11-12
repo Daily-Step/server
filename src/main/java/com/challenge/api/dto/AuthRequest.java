38df2f75e9f2c1c66bc5e5ae2e37636f3e3873f3
Eunjin3395 <jennyeunjin@khu.ac.kr>
2024-11-13 00:09:28 +0900

    style: 코드 format 수정

--- src/main/java/com/challenge/api/dto/AuthRequest.java
+++ src/main/java/com/challenge/api/dto/AuthRequest.java
@@ -3,14 +3,14 @@ package com.challenge.api.dto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-public class AuthRequest
+public class AuthRequest {
 
-{
-    
     @Getter
     @NoArgsConstructor
     public static class kakaoLoginRequest {
+
         String accessToken;
+
     }
-    
+
 }
