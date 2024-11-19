090aa0704e513f289fb9dfbf510ce5f5c0d7b8cb
GwonDooHyeon <secure3141@gmail.com>
2024-11-19 21:14:32 +0900

    CI/CD 테스트

--- src/main/java/com/challenge/config/SshTunnelingInitializer.java
+++ src/main/java/com/challenge/config/SshTunnelingInitializer.java
@@ -22,7 +22,7 @@ import static java.lang.System.exit;
 @Validated // 필드 유효성 검사
 @Setter
 public class SshTunnelingInitializer {
-    
+
     @NotNull
     private String remoteJumpHost;
     @NotNull
@@ -35,16 +35,21 @@ public class SshTunnelingInitializer {
     private String databaseUrl;
     @NotNull
     private int databasePort;
-    
+
     private Session session;
-    
+
     @PreDestroy // 빈이 삭제될 때 호출
     public void closeSSH() {
+        if (session == null) {
+            log.error("SSH session is not initialized. Please ensure the local profile is active.");
+            throw new IllegalStateException("SSH session is not initialized.");
+        }
+        
         if (session.isConnected()) {
             session.disconnect(); // ssh 연결 종료
         }
     }
-    
+
     /**
      * ssh 터널링 생성 및 포트 반환
      *
@@ -52,39 +57,39 @@ public class SshTunnelingInitializer {
      */
     public Integer buildSshConnection() {
         Integer forwardedPort = null;
-        
+
         try {
             log.info("{}@{}:{}:{} with privateKey", user, remoteJumpHost, sshPort, databasePort);
-            
+
             log.info("start ssh tunneling..");
             JSch jSch = new JSch();
-            
+
             log.info("creating ssh session");
             jSch.addIdentity(privateKey);  // private key 파일 추가
             session = jSch.getSession(user, remoteJumpHost, sshPort);  // ssh 세션 설정
-            
+
             Properties config = new Properties();
             config.put("StrictHostKeyChecking", "no"); // 서버 호스트 키 검증 비활성화
             session.setConfig(config);
             log.info("complete creating ssh session");
-            
+
             log.info("start connecting ssh connection");
             session.connect();  // ssh 연결
             log.info("success connecting ssh connection ");
-            
+
             // 로컬pc의 임의의 포트와 원격 접속한 pc의 db포트 연결
             log.info("start forwarding");
             forwardedPort = session.setPortForwardingL(0, databaseUrl, databasePort);
             log.info("successfully connected to database");
-            
+
         } catch (Exception e) {
             log.error("fail to make ssh tunneling");
             this.closeSSH(); // ssh 연결 해제
             e.printStackTrace();
             exit(1);
         }
-        
+
         return forwardedPort; // 포워딩된 포트 번호 반환
     }
-    
+
 }
