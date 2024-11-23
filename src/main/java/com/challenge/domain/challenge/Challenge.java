81455664a62920e9e7258bc17416daff0dbb1e9e
GwonDooHyeon <secure3141@gmail.com>
2024-11-23 22:07:07 +0900

    style: Wrapper -> Primitive 타입으로 변경

--- src/main/java/com/challenge/domain/challenge/Challenge.java
+++ src/main/java/com/challenge/domain/challenge/Challenge.java
@@ -3,8 +3,21 @@ package com.challenge.domain.challenge;
 import com.challenge.domain.BaseDateTimeEntity;
 import com.challenge.domain.category.Category;
 import com.challenge.domain.member.Member;
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -47,10 +60,10 @@ public class Challenge extends BaseDateTimeEntity {
     private boolean isDeleted;
 
     @Column(nullable = false)
-    private Integer weeklyGoalCount;
+    private int weeklyGoalCount;
 
     @Column(nullable = false)
-    private Integer totalGoalCount;
+    private int totalGoalCount;
 
     @Column(nullable = false)
     private LocalDateTime startDateTime;
