c203b1a391d6b29c81423fc844d838fa749c5a95
Eunjin3395 <jennyeunjin@khu.ac.kr>
2024-12-01 00:49:14 +0900

    test: 토큰 재발급 컨트롤러 테스트 코드 작성

--- src/main/java/com/challenge/api/controller/auth/request/ReissueTokenRequest.java
+++ src/main/java/com/challenge/api/controller/auth/request/ReissueTokenRequest.java
@@ -1,6 +1,7 @@
 package com.challenge.api.controller.auth.request;
 
 import com.challenge.api.service.auth.request.ReissueTokenServiceRequest;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,6 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class ReissueTokenRequest {
 
+    @NotBlank(message = "refresh token은 필수 입력값입니다.")
     private String refreshToken;
 
     public ReissueTokenServiceRequest toServiceRequest() {
