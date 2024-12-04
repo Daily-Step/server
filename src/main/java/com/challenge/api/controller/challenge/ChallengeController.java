76e00092cab8d2aaa32687f4f47750ba88cfa0a7
GwonDooHyeon <secure3141@gmail.com>
2024-12-04 20:59:08 +0900

    refactor: meberId -> member 넘기도록 수정

--- src/main/java/com/challenge/api/controller/challenge/ChallengeController.java
+++ src/main/java/com/challenge/api/controller/challenge/ChallengeController.java
@@ -8,7 +8,11 @@ import com.challenge.api.service.challenge.response.ChallengeResponse;
 import com.challenge.domain.member.Member;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -22,18 +26,15 @@ public class ChallengeController {
 
     @PostMapping("/challenges")
     public ApiResponse<ChallengeResponse> createChallenge(@RequestBody @Valid ChallengeCreateRequest request,
-                                                          @AuthMember Member member) {
-        Long memberId = member.getId();
+            @AuthMember Member member) {
         LocalDateTime startDateTime = LocalDateTime.now();
-        return ApiResponse.ok(challengeService.createChallenge(memberId, request.toServiceRequest(), startDateTime));
+        return ApiResponse.ok(challengeService.createChallenge(member, request.toServiceRequest(), startDateTime));
     }
 
     @GetMapping("/challenges")
     public ApiResponse<List<ChallengeResponse>> getChallenges(@AuthMember Member member) {
-        Long memberId = member.getId();
         LocalDateTime currentDateTime = LocalDateTime.now();
-        return ApiResponse.ok(challengeService.getChallenges(memberId, currentDateTime));
+        return ApiResponse.ok(challengeService.getChallenges(member, currentDateTime));
     }
-
 
 }
