297712f752b5f70d9d4929c8f33dfd06008cea47
GwonDooHyeon <secure3141@gmail.com>
2024-12-07 21:04:58 +0900

    feat: 챌린지 달성 테스트 코드 구현

--- src/test/java/com/challenge/api/service/challenge/ChallengeServiceTest.java
+++ src/test/java/com/challenge/api/service/challenge/ChallengeServiceTest.java
@@ -4,12 +4,19 @@ import com.challenge.api.service.challenge.request.ChallengeCreateServiceRequest
 import com.challenge.api.service.challenge.response.ChallengeResponse;
 import com.challenge.domain.category.Category;
 import com.challenge.domain.category.CategoryRepository;
+import com.challenge.domain.challenge.Challenge;
 import com.challenge.domain.challenge.ChallengeRepository;
 import com.challenge.domain.job.Job;
 import com.challenge.domain.job.JobRepository;
-import com.challenge.domain.member.*;
+import com.challenge.domain.member.Gender;
+import com.challenge.domain.member.JobYear;
+import com.challenge.domain.member.LoginType;
+import com.challenge.domain.member.Member;
+import com.challenge.domain.member.MemberRepository;
+import com.challenge.domain.record.Record;
+import com.challenge.domain.record.RecordRepository;
+import com.challenge.utils.DateUtils;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,16 +25,14 @@ import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.assertj.core.groups.Tuple.tuple;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class ChallengeServiceTest {
-
-    private Category category;
-    private Job job;
-    private Member member;
 
     @Autowired
     private MemberRepository memberRepository;
@@ -42,24 +47,14 @@ class ChallengeServiceTest {
     private ChallengeRepository challengeRepository;
 
     @Autowired
+    private RecordRepository recordRepository;
+
+    @Autowired
     private ChallengeService challengeService;
-
-    @BeforeEach
-    void setUp() {
-        category = Category.builder()
-                .name("카테고리")
-                .build();
-        categoryRepository.save(category);
-
-        job = Job.builder()
-                .code("코드")
-                .description("설명")
-                .build();
-        jobRepository.save(job);
-    }
 
     @AfterEach
     void tearDown() {
+        recordRepository.deleteAllInBatch();
         challengeRepository.deleteAllInBatch();
         memberRepository.deleteAllInBatch();
         jobRepository.deleteAllInBatch();
@@ -71,17 +66,14 @@ class ChallengeServiceTest {
     void createChallenge() {
         // given
         LocalDateTime startDateTime = LocalDateTime.of(2024, 11, 11, 10, 10, 30);
-        member = createMember();
+
+        Member member = createMember();
         memberRepository.save(member);
 
-        ChallengeCreateServiceRequest request = ChallengeCreateServiceRequest.builder()
-                .title("제목")
-                .durationInWeeks(2)
-                .weeklyGoalCount(3)
-                .categoryId(1L)
-                .color("색상")
-                .content("내용")
-                .build();
+        Category category = createCategory();
+        categoryRepository.save(category);
+
+        ChallengeCreateServiceRequest request = createChallengeServiceRequest();
 
         // when
         ChallengeResponse challengeResponse = challengeService.createChallenge(member, request, startDateTime);
@@ -97,26 +89,85 @@ class ChallengeServiceTest {
                 .contains("2024-11-11 10:10:30", 6);
     }
 
-    @DisplayName("오늘 날짜에 대해 챌린지를 달성 한다.")
+    @DisplayName("챌린지를 달성 한다.")
     @Test
-    void successChallenge() throws Exception {
+    void successChallenge() {
         // given
+        Member member = createMember();
+        memberRepository.save(member);
+
+        Category category = createCategory();
+        categoryRepository.save(category);
+
+        ChallengeCreateServiceRequest request = createChallengeServiceRequest();
+
+        Challenge challenge = Challenge.create(member, category, request, LocalDateTime.of(2024, 11, 11, 10, 10, 30));
+        challengeRepository.save(challenge);
+
+        Record record1 = createRecord(challenge, LocalDate.of(2024, 11, 11));
+        Record record2 = createRecord(challenge, LocalDate.of(2024, 11, 12));
+        Record record3 = createRecord(challenge, LocalDate.of(2024, 11, 13));
+        recordRepository.saveAll(List.of(record1, record2, record3));
 
         // when
+        ChallengeResponse challengeResponse = challengeService.successChallenge(
+                challenge.getId(),
+                LocalDate.of(2024, 11, 14)
+        );
 
         // then
+        assertThat(challengeResponse.getId()).isNotNull();
+        assertThat(challengeResponse.getTitle()).isEqualTo("제목");
+        assertThat(challengeResponse.getRecords()).hasSize(4)
+                .extracting("id", "successDate")
+                .containsExactlyInAnyOrder(
+                        tuple(1L, DateUtils.toDayString(LocalDate.of(2024, 11, 11))),
+                        tuple(2L, DateUtils.toDayString(LocalDate.of(2024, 11, 12))),
+                        tuple(3L, DateUtils.toDayString(LocalDate.of(2024, 11, 13))),
+                        tuple(4L, DateUtils.toDayString(LocalDate.of(2024, 11, 14)))
+                );
     }
 
     private Member createMember() {
+        Job job = Job.builder()
+                .code("1")
+                .description("1")
+                .build();
+        jobRepository.save(job);
+
         return Member.builder()
                 .socialId(1L)
-                .email("email")
+                .email("eamil")
                 .loginType(LoginType.KAKAO)
                 .nickname("nickname")
-                .birth(LocalDate.of(1999, 1, 1))
+                .birth(LocalDate.of(2000, 1, 1))
                 .gender(Gender.MALE)
                 .jobYear(JobYear.LT_1Y)
                 .job(job)
+                .build();
+    }
+
+    private Category createCategory() {
+        return Category.builder()
+                .name("카테고리")
+                .build();
+    }
+
+    private Record createRecord(Challenge challenge, LocalDate currentDate) {
+        return Record.builder()
+                .challenge(challenge)
+                .successDate(currentDate)
+                .build();
+    }
+
+    private ChallengeCreateServiceRequest createChallengeServiceRequest() {
+        return ChallengeCreateServiceRequest.builder()
+                .title("제목")
+                .durationInWeeks(2)
+                .weeklyGoalCount(3)
+                .categoryId(1L)
+                .color("색상")
+                .content("내용")
                 .build();
     }
 
