136c6a1812db8d6190ebdb409ca800345fc0f618
GwonDooHyeon <secure3141@gmail.com>
2024-12-07 20:15:24 +0900

    feat: QueryDsl 빈 주입

--- src/main/java/com/challenge/ChallengeApplication.java
+++ src/main/java/com/challenge/ChallengeApplication.java
@@ -1,5 +1,7 @@
 package com.challenge;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -15,6 +17,11 @@ public class ChallengeApplication {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    JPAQueryFactory jpaQueryFactory(EntityManager em) {
+        return new JPAQueryFactory(em);
     }
 
 }
