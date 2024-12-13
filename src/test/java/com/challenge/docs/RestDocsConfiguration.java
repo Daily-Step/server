37d32008011af9033a392af8e8776897305604de
Eunjin3395 <jennyeunjin@khu.ac.kr>
2024-12-13 15:53:21 +0900

    docs: restDocs 문서 작성 공통화

---
+++ src/test/java/com/challenge/docs/RestDocsConfiguration.java
@@ -0,0 +1,34 @@
+package com.challenge.docs;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
+import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
+import org.springframework.restdocs.snippet.Attributes;
+
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.modifyHeaders;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+
+@TestConfiguration
+public class RestDocsConfiguration {
+
+    @Bean
+    public RestDocumentationResultHandler restDocumentationResultHandler() {
+        return MockMvcRestDocumentation.document(
+                "{class-name}/{method-name}", // 문서 이름 설정
+                preprocessRequest(  // 공통 헤더 설정
+                        modifyHeaders()
+                                .remove("Content-Length")
+                                .remove("Host"),
+                        prettyPrint()),  // pretty json 적용
+                preprocessResponse(prettyPrint())    // pretty json 적용
+        );
+    }
+
+    public static Attributes.Attribute field(final String value) {
+        return new Attributes.Attribute("constraints", value);
+    }
+
+}
