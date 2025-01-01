9569f1ab0d7078ba67fc9f9ca5393afb11ad874c
GwonDooHyeon <secure3141@gmail.com>
2025-01-01 18:03:36 +0900

    docs: 챌린지 조회 문서 작성

--- src/main/java/com/challenge/api/controller/challenge/request/ChallengeQueryRequest.java
+++ src/main/java/com/challenge/api/controller/challenge/request/ChallengeQueryRequest.java
@@ -2,6 +2,7 @@ package com.challenge.api.controller.challenge.request;
 
 import com.challenge.api.service.challenge.request.ChallengeQueryServiceRequest;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -11,6 +12,11 @@ public class ChallengeQueryRequest {
 
     @NotBlank(message = "조회일은 필수 입력 값입니다.")
     private String queryDate;
+
+    @Builder
+    private ChallengeQueryRequest(String queryDate) {
+        this.queryDate = queryDate;
+    }
 
     public ChallengeQueryServiceRequest toServiceRequest() {
         return ChallengeQueryServiceRequest.builder()
