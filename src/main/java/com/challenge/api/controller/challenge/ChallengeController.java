9606678702d5f157dd659ed6b67055e007501412
GwonDooHyeon <secure3141@gmail.com>
2025-01-01 19:41:10 +0900

    docs: 챌린지 달성 취소 문서 작성

--- src/main/java/com/challenge/api/controller/challenge/ChallengeController.java
+++ src/main/java/com/challenge/api/controller/challenge/ChallengeController.java
@@ -31,6 +31,9 @@ public class ChallengeController {
 
     private final ChallengeService challengeService;
 
+    /**
+     * 입력 받은 날짜 기준 이전 2달간의 챌린지 목록 조회 (2달전 00:00:00 ~ 입력받은 날짜 23:59:59)
+     */
     @GetMapping("/challenges")
     public ApiResponse<List<ChallengeResponse>> getChallenges(
             @AuthMember Member member,
@@ -38,6 +41,9 @@ public class ChallengeController {
         return ApiResponse.ok(challengeService.getChallenges(member, request.toServiceRequest()));
     }
 
+    /**
+     * 챌린지 생성
+     */
     @PostMapping("/challenges")
     public ApiResponse<ChallengeResponse> createChallenge(
             @AuthMember Member member,
@@ -46,6 +52,9 @@ public class ChallengeController {
         return ApiResponse.ok(challengeService.createChallenge(member, request.toServiceRequest(), startDateTime));
     }
 
+    /**
+     * 챌린지 달성
+     */
     @PostMapping("/challenges/{challengeId}/achieve")
     public ApiResponse<ChallengeResponse> achieveChallenge(
             @AuthMember Member member,
@@ -54,6 +63,9 @@ public class ChallengeController {
         return ApiResponse.ok(challengeService.achieveChallenge(member, challengeId, request.toServiceRequest()));
     }
 
+    /**
+     * 챌린지 달성 취소
+     */
     @PostMapping("/challenges/{challengeId}/cancel")
     public ApiResponse<ChallengeResponse> cancelChallenge(
             @AuthMember Member member,
@@ -62,6 +74,9 @@ public class ChallengeController {
         return ApiResponse.ok(challengeService.cancelChallenge(member, challengeId, request.toServiceRequest()));
     }
 
+    /**
+     * 챌린지 수정
+     */
     @PutMapping("/challenges/{challengeId}")
     public ApiResponse<ChallengeResponse> updateChallenge(
             @AuthMember Member member,
@@ -70,6 +85,9 @@ public class ChallengeController {
         return ApiResponse.ok(challengeService.updateChallenge(member, challengeId, request.toServiceRequest()));
     }
 
+    /**
+     * 챌린지 삭제
+     */
     @DeleteMapping("/challenges/{challengeId}")
     public ApiResponse<Long> deleteChallenge(@AuthMember Member member, @PathVariable Long challengeId) {
         return ApiResponse.ok(challengeService.deleteChallenge(member, challengeId));
