29bad4ae3df1986f3086ac0cda2c30f2af4d6d62
GwonDooHyeon <secure3141@gmail.com>
2025-01-01 14:12:08 +0900

    fix: 챌린지 생성시 카테고리아이디가 null로 들어오는 버그 수정

--- src/main/java/com/challenge/api/controller/challenge/request/ChallengeCreateRequest.java
+++ src/main/java/com/challenge/api/controller/challenge/request/ChallengeCreateRequest.java
@@ -6,6 +6,7 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -34,11 +35,23 @@ public class ChallengeCreateRequest {
     @Size(max = 500, message = "상세 내용은 공백 포함 최대 500자 이하여야 합니다.")
     private String content;
 
+    @Builder
+    private ChallengeCreateRequest(String title, int durationInWeeks, int weeklyGoalCount, Long categoryId,
+            String color, String content) {
+        this.title = title;
+        this.durationInWeeks = durationInWeeks;
+        this.weeklyGoalCount = weeklyGoalCount;
+        this.categoryId = categoryId;
+        this.color = color;
+        this.content = content;
+    }
+
     public ChallengeCreateServiceRequest toServiceRequest() {
         return ChallengeCreateServiceRequest.builder()
                 .title(title)
                 .durationInWeeks(durationInWeeks)
                 .weeklyGoalCount(weeklyGoalCount)
+                .categoryId(categoryId)
                 .color(color)
                 .content(content)
                 .build();
